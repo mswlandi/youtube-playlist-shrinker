159c3d290985af05a3805b698158d5a307a261eb
Frederico-Messa <frederico.messa@gmail.com>
2020-08-20 23:27:27 -0300

    Fix bug + set default + parameters by input

--- shrink-playlist.py
+++ shrink-playlist.py
@@ -3,43 +3,70 @@ import re
 import os
 import sys, getopt
 
+def editDefaultProgramParameterValue(parameter, value):
+    programLines = open(__file__).read().split('\n')
+    with open(__file__, 'w') as programFile:
+        newProgramLines = []
+        print(__file__)
+        for line in programLines:
+            if parameter + ' = \'' in line:
+                newProgramLines.append(line.split('=')[0] + '= \'' + value + '\'')
+            else:
+                newProgramLines.append(line)
+        programFile.write('\n'.join(newProgramLines))
+
 try:
+    l = sys.argv[1]
     opts, args = getopt.getopt(sys.argv[2:],"hv:s:m:",["sounded_speed=", "silent_speed=", "margin="])
-    s = '10'
-    v = '1.25'
+    s = '7'
+    v = '1.4'
     m = '2'
-except getopt.GetoptError:
-    print('shrink-playlist.py <inputfile> -o <outputfile> -v <sounded speed> -s <silent speed> -m <margin>')
-    sys.exit(2)
+except:
+    try:
+        parameters = input('Parameters: ')
+        l = parameters.split(' ')[0]
+        opts, args = getopt.getopt(parameters.split(' ')[1:],"hv:s:m:",["sounded_speed=", "silent_speed=", "margin="])
+        s = '7'
+        v = '1.4'
+        m = '2'
+    except:
+        print('shrink-playlist.py <inputfile> -v <sounded speed> -s <silent speed> -m <margin>')
+        sys.exit(2)
 
 for opt, arg in opts:
     if opt == '-h':
-        print('shrink-playlist.py <inputfile> -o <outputfile> -v <sounded speed> -s <silent speed> -m <margin>')
+        print('shrink-playlist.py <inputfile> -v <sounded speed> -s <silent speed> -m <margin>')
         sys.exit()
     elif opt in ("-v", "--sounded_speed"):
         v = arg
+        editDefaultProgramParameterValue('v', v)
     elif opt in ("-s", "--silent_speed"):
         s = arg
+        editDefaultProgramParameterValue('s', s)
     elif opt in ("-m", "--margin"):
         m = arg
+        editDefaultProgramParameterValue('m', m)
 
-playlist = Playlist(sys.argv[1])
-titles = []
+playlist = Playlist(l)
 
 arguments = f'-v {v} -s {s} -m {m}'
 
 # YouTube updated their HTML so the regex that Playlist uses to find the videos is currently outdated
 playlist._video_regex = re.compile(r"\"url\":\"(/watch\?v=[\w-]*)")
 
-print(f'downloading {len(playlist.video_urls)} videos from {playlist.title()}:\n')
+currentDirs = [p for p in os.listdir() if os.path.isdir(p)]
+
+print(f'\nDownloading {len(playlist.video_urls)} videos from {playlist.title()}:\n')
 for url in playlist.video_urls:
     print(url)
     video = YouTube(url)
-    titles.append(video.title)
     video.streams.get_highest_resolution().download(playlist.title())
 
-for title in titles:
-    print(f"\nshrinking {title}")
-    os.system(f'auto-editor "{playlist.title()}/{title}.mp4" {arguments} --no_open -o "{playlist.title()}/{title}_t.mp4"')
-    os.remove(f'{playlist.title()}/{title}.mp4')
-    os.rename(f'{playlist.title()}/{title}_t.mp4', f'{playlist.title()}/{title}.mp4')
+folder = [p for p in os.listdir() if os.path.isdir(p) and p not in currentDirs][0]
+
+for video in os.listdir(folder):
+    videoName = '.'.join(video.split('.')[:-1])
+    print(f'\nShrinking {videoName}:\n')
+    os.system(f'auto-editor "{folder}/{video}" {arguments} --no_open -o "_{folder}/{video}"')
+    os.remove(f'{folder}/{video}')
+    os.rename(f'_{folder}/{video}', f'{folder}/{video}')
