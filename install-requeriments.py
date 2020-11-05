d2b282b8b87f3d19f58e2cca166546dba7a7e0df
Frederico-Messa <frederico.messa@gmail.com>
2020-11-05 17:26:05 -0300

    install requeriments updated and fixed

--- install-requeriments.py
+++ install-requeriments.py
@@ -1,3 +1,5 @@
 import subprocess
+subprocess.run('pip3 uninstall -y pytube')
 subprocess.run('pip3 uninstall -y auto-editor')
-subprocess.run('pip3 install -U auto-editor==20.33.1.0')
+subprocess.run('pip3 install -U auto-editor==20.43.2.0')
+subprocess.run('pip3 install -U pytube==9.7.0')
