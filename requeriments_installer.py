0d1925be6dfa75ab6d9aa2fb26a52a2f21b144a8
MSWLandi <38134398+mswlandi@users.noreply.github.com>
2021-01-28 13:58:34 -0300

    new safe version for new installs and updates

--- requeriments_installer.py
+++ requeriments_installer.py
@@ -1,5 +1,5 @@
 import subprocess
 subprocess.run('pip3 uninstall -y pytube')
 subprocess.run('pip3 uninstall -y auto-editor')
-subprocess.run('pip3 install -U auto-editor==20.43.2.0')
-subprocess.run('pip3 install -U pytube==9.7.0')
+subprocess.run('pip3 install -U auto-editor==21.4.1')
+subprocess.run('pip3 install -U pytube==10.4.1')
